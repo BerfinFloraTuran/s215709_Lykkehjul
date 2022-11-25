7c6399a5027ef325cc327f1eaa5f01e7adc1cf01
Flora <s215709@student.dtu.dk>
2022-11-25 22:50:01 +0000

    fixing build error
    adding activity to manifest

--- app/src/main/java/com/example/a215709_lykkehjul/View/MainActivity.kt
+++ app/src/main/java/com/example/a215709_lykkehjul/View/MainActivity.kt
@@ -1,17 +1,14 @@
 package com.example.a215709_lykkehjul.View
 
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            //Nedenstående linje 14 fra stackoverflow
-            //window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
             NavController()
         }
     }
