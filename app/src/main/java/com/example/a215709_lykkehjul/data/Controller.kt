2a49e221ed79fbe370782a2167c1abc71e706fb9
Flora <s215709@student.dtu.dk>
2022-11-26 23:56:31 +0000

    updating categories

--- app/src/main/java/com/example/a215709_lykkehjul/data/Controller.kt
+++ app/src/main/java/com/example/a215709_lykkehjul/data/Controller.kt
@@ -14,11 +14,11 @@ class Controller {
     fun initCategoryData(){
         val animals = Category("Animals", listOf(Word("Tiger"), Word("Horse"), Word("Koala"), Word("Dog")))
         val countries = Category("Countries", listOf(Word("Turkey"), Word("Denmark"), Word("Kurdistan"), Word("Palestine")))
-        val boybands = Category("Boy-Bands", listOf(Word("One Direction"), Word("NSYNC"), Word("Backstreet Boys"), Word("BTS")))
+        val programming_languages = Category("Programming Languages", listOf(Word("Java"), Word("Kotlin"), Word("SQL"), Word("Python")))
         val instruments = Category("Instruments", listOf(Word("Piano"), Word("Guitar"), Word("Ukulele"), Word("Triangle")))
 
 
-        val categories = listOf(animals,countries,boybands,instruments)
+        val categories = listOf(animals,countries,programming_languages,instruments)
 
         for (category in categories){
             categoryData.categories.add(category)
